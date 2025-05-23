0c17a44b0f2a5c1b3f37f801aa9b8922b14b4ddd
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2025-05-23 13:39:50 +0300

    Setup Tailwind

--- app/(tabs)/dashboard.jsx
+++ app/(tabs)/dashboard.jsx
@@ -1,12 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
 
 export default function dashboard() {
   return (
     <View>
       <Text>dashboard</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function index() {
   return (
-    <View>
-      <Text>index Page is here</Text>
+    <View className="flex-1 items-center justify-centerbg-black">
+      <Text className="text-red-500">index Page is here</Text>
     </View>
   );
 }
